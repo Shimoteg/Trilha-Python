49ac46e0627c0339081f386337d94962577c84f0
Shimoteg <gabrielshimote@outlook.com>
2024-10-08 17:51:58 -0300

    range() - len() - enumerate() no loop

--- Modulos/loop.py
+++ Modulos/loop.py
@@ -29,3 +29,38 @@ for valor in pessoa.values():
 print("For utilizando dicionario get")
 for chave, valor in pessoa.items():
     print(f"{chave}: {valor}")
+
+#Range(): interalo numerico // retorna em lista os valores declarados
+print("\n Usando a funçao range() e transformando em lista: ")
+range(0, 10)
+print(list(range(0,10)))
+
+#Short hand =  a função range poide ser dado o valor final que comeca em 0
+print("\n Exemplo de range usando apenas o valor final (50): ")
+print(list(range(50)))
+
+#Utilizando a funcao len() junto com a range() e possivel mensurar o tamanho da lista *Esta maneira e utilizada pois permite alteracao
+print("\n Utilizando a funçao len() junto com a range(): ")
+lista = [0, 1, 2, 3, 4, 5]
+for indice in range(0, len(lista)):
+    print("Indice: ", indice)
+    print("Elemento: ", lista[indice])
+
+#Realizando alteracao na lista
+print("\n Realizando alteracao na lista: ")
+print(lista)
+for indice in range(0, len(lista)):
+    if indice == 3:
+        lista[indice] = 100
+    else:
+        lista[indice] = 0
+print("\n Apos a alteracao: ")
+print(lista)
+
+#Enumerate(): permite retornar o indice e o elemento da lista
+lista_enumerate = ["a", "b", "c"]	
+print("\n Usando enumerate(): ")
+for indice, valor in enumerate(lista_enumerate):
+    print(f"{indice}: {valor}")
+    if indice == 1:
+        print("Indice 2")
